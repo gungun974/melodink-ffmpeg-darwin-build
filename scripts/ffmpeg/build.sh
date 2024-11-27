8a75aeb89dc710993d1784c43c92c797286e0345
Gungun974 <xfelix974@gmail.com>
2024-11-28 01:03:52 +0400

    Add hls seek patch for ffmpeg

--- scripts/ffmpeg/build.sh
+++ scripts/ffmpeg/build.sh
@@ -6,9 +6,10 @@ set -u # treat unset variables as an error
 cd ${SRC_DIR}
 
 patch -p1 <${PROJECT_DIR}/patches/ffmpeg-fix-vp9-hwaccel.patch
-#patch -p1 <${PROJECT_DIR}/patches/ffmpeg-fix-hls-mp4-seek.patch
 patch -p1 <${PROJECT_DIR}/patches/ffmpeg-fix-ios-hdr-texture.patch
 patch -p1 <${PROJECT_DIR}/patches/ffmpeg-fix-dash-base-url-escape.patch
+patch -p1 <${PROJECT_DIR}/patches/ffmpeg-hls-seek-patch-1.patch
+patch -p1 <${PROJECT_DIR}/patches/ffmpeg-hls-seek-patch-2.patch
 
 cp ${PROJECT_DIR}/scripts/ffmpeg/meson.* .
 
